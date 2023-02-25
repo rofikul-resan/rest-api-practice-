b20fc9e59efe164a85b62153e4e73f2359ac724e
rofikul-resan <resan6231@gmail.com>
2023-02-25 20:50:27 +0600

    api 1 ui done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("add");
+// api 1 ui display 
+
+function api1UI(){
+    const {result}= person;
+    const cardContenter = document.getElementById("cardContenter");
+result.forEach(singlePerson => {
+    const card = document.createElement("div");
+    card.classList.add("col-sm-6");
+    card.innerHTML= `
+    <div class = "card shadow-lg">
+    <div class="card-header">
+        Person name : ${singlePerson.name.common}
+        </div>
+        <div class="card-body">
+            <p class="card-title">age : ${singlePerson.age} </p>
+            <p class="card-text">Street : ${singlePerson.address.street}</p>
+        </div>
+    </div>
+    `
+    cardContenter.appendChild(card);
+})
+console.log(person);
+}
+api1UI()
