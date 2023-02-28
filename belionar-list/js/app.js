8654ebae955615d07560db4b047079af4caadbdc
rofikul-resan <resan6231@gmail.com>
2023-03-01 01:08:38 +0600

    top ten display in list

--- belionar-list/js/app.js
+++ belionar-list/js/app.js
@@ -1,9 +1,16 @@
 function displayInTable(data){
     console.log("c");
+    const tbody = document.getElementById("tbody");
     data.forEach(element => {
+        const {person, countryOfCitizenship, rank, industries, finalWorth} = element
+        const tr = document.createElement("tr");
+        tr.innerHTML=`
+        <td> ${person.name}</td>
+        <td> ${countryOfCitizenship}</td>
+        <td> ${industries}</td>
+        <td> ${rank}</td>
+        <td>$ ${finalWorth}</td>`
+        tbody.appendChild(tr)
         console.log(element);
     });
-    const tr = document.createElement("tr");
-    tr.innerHTML=`
-    `
 }
--- belionar-list/js/fatch-data.js
+++ belionar-list/js/fatch-data.js
@@ -2,5 +2,5 @@ function topTenRank(){
     fetch("https://forbes400.onrender.com/api/forbes400?limit=10")
     .then(res => res.json())
     .then(data => displayInTable(data))
-    log
+    
 }
